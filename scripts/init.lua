c5bbeb8b47e202005db1b3cb24b989af1aca24e8
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2024-04-07 02:33:17 -0700

    Huge push for a working product

---
+++ scripts/init.lua
@@ -0,0 +1,63 @@
+local variant = Tracker.ActiveVariantUID
+
+-- Logic
+ScriptHost:LoadScript("scripts/logic/logic.lua")
+
+-- Items
+print("Loading Items")
+Tracker:AddItems("items/items.json")
+Tracker:AddItems("items/keys.json")
+Tracker:AddItems("items/artifacts.json")
+-- Markers
+Tracker:AddItems("items/markers/house.json")
+Tracker:AddItems("items/markers/second.json")
+Tracker:AddItems("items/markers/daycare.json")
+Tracker:AddItems("items/markers/mirrorgasstation.json")
+Tracker:AddItems("items/markers/emptyhouse.json")
+Tracker:AddItems("items/markers/hospital.json")
+print("")
+
+-- Maps
+print("Loading Maps")
+Tracker:AddMaps("variants/" .. variant .. "/maps/maps.json")
+print("")
+
+-- Grids
+print("Loading Grids")
+Tracker:AddLayouts("layouts/grids/weapons.json")
+Tracker:AddLayouts("layouts/grids/keys.json")
+Tracker:AddLayouts("layouts/grids/artifacts.json")
+Tracker:AddLayouts("layouts/grids/markers.json")
+print("")
+
+-- Variant
+print("Loading Variant")
+Tracker:AddLayouts("variants/" .. variant .. "/layouts/tabs/myhouse.json")
+Tracker:AddLayouts("variants/" .. variant .. "/layouts/tabs/underhalls.json")
+Tracker:AddLayouts("variants/" .. variant .. "/layouts/tabs.json")
+Tracker:AddLayouts("variants/" .. variant .. "/layouts/tracker.json")
+print("")
+
+-- Base Layouts
+print("Loading Base Layouts")
+Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/broadcast.json")
+print("")
+
+-- Locations
+print("Loading Locations")
+if variant == "maps" then
+    Tracker:AddLocations("variants/" .. variant .. "/locations/underworld/thehouse.json")
+    Tracker:AddLocations("variants/" .. variant .. "/locations/underworld/underhalls.json")
+    Tracker:AddLocations("variants/" .. variant .. "/locations/underworld/secondhouse.json")
+    Tracker:AddLocations("variants/" .. variant .. "/locations/underworld/mirrorhouse.json")
+    Tracker:AddLocations("variants/" .. variant .. "/locations/underworld/daycare.json")
+    Tracker:AddLocations("variants/" .. variant .. "/locations/underworld/floodedhouse.json")
+    Tracker:AddLocations("variants/" .. variant .. "/locations/underworld/bathhouse.json")
+    Tracker:AddLocations("variants/" .. variant .. "/locations/underworld/brutalisthouse.json")
+    Tracker:AddLocations("variants/" .. variant .. "/locations/underworld/airport.json")
+    Tracker:AddLocations("variants/" .. variant .. "/locations/underworld/gasstation.json")
+    Tracker:AddLocations("variants/" .. variant .. "/locations/underworld/mirrorgasstation.json")
+    Tracker:AddLocations("variants/" .. variant .. "/locations/underworld/emptyhouse.json")
+    Tracker:AddLocations("variants/" .. variant .. "/locations/underworld/hospital.json")
+end
